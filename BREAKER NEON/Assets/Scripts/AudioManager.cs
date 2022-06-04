2c99aa0b8298c80f35a05b51c0f29695ffb3a186
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-06-04 12:49:16 -0300

    sound controller was added

---
+++ BREAKER NEON/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField] AudioMixer mixer;
+
+    //para guardar y cargar datos del playerPrefs
+    public const string MUSIC_KEY = "musicExpose";
+    public const string SFX_KEY = "sfxExpose";
+    
+    //singleton para su uso continuo
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        loadVolume();
+    }
+    private void loadVolume()
+    {
+        float music = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfx = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+        mixer.SetFloat(VolumenSettings.MUSIX_EXPOSE, Mathf.Log10(music) * 20);
+        mixer.SetFloat(VolumenSettings.SFX_EXPOSE, Mathf.Log10(sfx) * 20);
+    }
+}
---
+++ BREAKER NEON/Assets/Scripts/VolumenSettings.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class VolumenSettings : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider sliderMusic;
+    [SerializeField] Slider sliderSfx;
+    public const string MUSIX_EXPOSE = "musicExpose";
+    public const string SFX_EXPOSE = "sfxExpose";
+    
+    private void Awake()
+    {
+        sliderMusic.onValueChanged.AddListener(setMusicFloat);
+        sliderSfx.onValueChanged.AddListener(setSfxFloat);
+    }
+    //desarrollo las funciones para establecer los valores de los sliders
+    void setMusicFloat(float sliderMusic)
+    {
+        mixer.SetFloat(MUSIX_EXPOSE, Mathf.Log10(sliderMusic) * 20);
+    }
+    void setSfxFloat(float sliderSfx)
+    {
+        mixer.SetFloat(SFX_EXPOSE, Mathf.Log10(sliderSfx) * 20);
+    }
+
+    //cargamos los valores que disponibles en el playerPrefs
+    void Start()
+    {
+        sliderMusic.value= PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY,1f);
+        sliderSfx.value= PlayerPrefs.GetFloat(AudioManager.SFX_KEY,1f);
+    }
+    //guarda los valores de los sliders en el playerPrefs
+    private void OnDisable() {
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY,sliderMusic.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY,sliderSfx.value);     
+    }
+}
